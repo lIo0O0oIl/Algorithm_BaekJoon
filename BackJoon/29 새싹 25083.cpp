0ab7ff48861ead9a5eb5d68610685e4bb6ee7c28
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2023-12-29 23:43:19 +0900

    2508 새싹 풂

---
+++ BackJoon/29 새싹 25083.cpp
@@ -0,0 +1,13 @@
+// https://www.acmicpc.net/problem/25083
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "         ,r'\"7" << endl;
+	cout << "r`-_   ,'  ,/" << endl;
+	cout << " \\. \". L_r'" << endl;
+	cout << "   `~\\/" << endl;
+	cout << "      |" << endl;
+	cout << "      |" << endl;
+}
