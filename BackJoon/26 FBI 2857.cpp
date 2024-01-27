022b6d3620bb0f39aeaa1f89fb2c35c22131e1b0
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2024-01-27 19:38:57 +0900

    카드 게임 풂

---
+++ BackJoon/26 FBI 2857.cpp
@@ -0,0 +1,22 @@
+// https://www.acmicpc.net/problem/2857
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr); cout.tie(nullptr);
+
+	string input;
+	bool FBI = false;
+	for (int i = 0; i < 5; i++)
+	{
+		cin >> input;
+		if (input.find("FBI") != string::npos)
+		{
+			cout << i + 1 << " ";
+			FBI = true;
+		}
+	}
+	if (!FBI) cout << "HE GOT AWAY!";
+}
