d7bc1a2f2c769e4e454b938044cef2a5f7589312
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2024-02-04 22:47:55 +0900

    너의 평점은 풂

---
+++ BackJoon/콜센터 5339.cpp
@@ -0,0 +1,18 @@
+﻿// https://www.acmicpc.net/problem/5339
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "     /~\\" << '\n';
+    cout << "    ( oo|" << '\n';
+    cout << "    _\\=/_" << '\n';
+    cout << "   /  _  \\" << '\n';
+    cout << "  //|/.\\|\\\\ " << '\n';
+    cout << " ||  \\ /  ||" << '\n';
+    cout << "============" << '\n';
+    cout << "|          |" << '\n';
+    cout << "|          |" << '\n';
+    cout << "|          |" << '\n';
+}​
