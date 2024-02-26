8e505303427e41cf476d2dc5fd3c74267be63966
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2024-02-26 11:09:47 +0900

    수 정렬하기 풂

---
+++ BackJoon/26 수 정렬하기 2750.cpp
@@ -0,0 +1,21 @@
+// https://www.acmicpc.net/problem/2750
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int arr[1001];
+
+int main() {
+	int n, input;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+	sort(&arr[0], &arr[n]);
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << '\n';
+	}
+}
