dfd2a69a759b440c06fc6407d1b3f91816c3e3f0
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2024-02-28 21:24:32 +0900

    Hook 풂

---
+++ BackJoon/28 Hook 10189.cpp
@@ -0,0 +1,12 @@
+// https://www.acmicpc.net/problem/10189
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "#  # #### #### #  #" << '\n';
+    cout << "#### #  # #  # # #" << '\n';
+    cout << "#### #  # #  # # #" << '\n';
+    cout << "#  # #### #### #  #";
+}
