27ba8ac7b97173694bbb37a07dfe871f151e7714
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2024-03-01 18:37:45 +0900

    Serca 풂

---
+++ BackJoon/30 Serca 26766.cpp
@@ -0,0 +1,22 @@
+// https://www.acmicpc.net/problem/26766
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cin >> n;
+
+    while (n--) {
+        cout << " @@@   @@@ " << '\n';
+        cout << "@   @ @   @" << '\n';
+        cout << "@    @    @" << '\n';
+        cout << "@         @" << '\n';
+        cout << " @       @ " << '\n';
+        cout << "  @     @  " << '\n';
+        cout << "   @   @   " << '\n';
+        cout << "    @ @    " << '\n';
+        cout << "     @     " << '\n';
+    }
+}
