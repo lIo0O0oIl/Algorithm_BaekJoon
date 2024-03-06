49aee73169388ba1c9a4ff186e5135567559d475
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2024-03-06 14:35:46 +0900

    Can you add this? 풂

---
+++ BackJoon/6 Can you add this 7891.cpp
@@ -0,0 +1,15 @@
+// https://www.acmicpc.net/problem/7891
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int a, b;
+        cin >> a >> b;
+        cout << a + b << '\n';
+    }
+}
