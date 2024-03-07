88cdf6a557c4756dd9c1673cb3a135734df847e2
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2024-03-07 23:41:17 +0900

    음계 풂

---
+++ BackJoon/7 음계 2920.cpp
@@ -0,0 +1,28 @@
+// https://www.acmicpc.net/problem/2920
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[8];
+    string str;
+
+    for (int i = 0; i < 8; ++i)
+        cin >> arr[i];
+
+    for (int i = 0; i < 8; ++i)
+    {
+        if (arr[i] == i + 1 && str != "descending")
+            str = "ascending";
+        else if (arr[i] == 8 - i && str != "ascending")
+            str = "descending";
+        else
+        {
+            str = "mixed";
+            break;
+        }
+    }
+
+    cout << str;
+}
