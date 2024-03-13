b17708b05e718dd27188997dd03dab9c714d7917
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2024-03-13 11:47:46 +0900

    Plane 풂

---
+++ BackJoon/13 Plane 8370.cpp
@@ -0,0 +1,11 @@
+// https://www.acmicpc.net/problem/8370
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    long long n1, n2, k1, k2;
+    cin >> n1 >> k1 >> n2 >> k2;
+    cout << n1 * k1 + n2 * k2;
+}
