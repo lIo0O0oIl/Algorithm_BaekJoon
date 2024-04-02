e686a85647a9a15c5ccaa2a5fce4b18515b27e7c
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2024-04-02 14:35:18 +0900

    부녀회장이 될테야 풂

--- BackJoon/2 부녀회장이 될테야 2775.cpp
+++ BackJoon/2 부녀회장이 될테야 2775.cpp
@@ -4,25 +4,28 @@
 using namespace std;
 
 int arr[15][15];
+int t, k, n;		// 테스트 케이스, 층, 호
 
-int DP(int k, int n) {
-	if (arr[k][n] == 0)
+int DP(int _k, int _n) {
+	if (arr[_k][_n] == 0)
 	{
-		for (int i = 0; i < n; i++)
+		for (int i = 1; i <= _n; i++)
 		{
-			arr[k][n] += DP(k - 1, i);
+			arr[_k][_n] += DP(_k - 1, i);
 		}
 	}
-	return arr[k][n];
+	return arr[_k][_n];
 }
 
 int main() {
-	int t, k, n;		// 테스트 케이스, 층, 호
-	cin >> t;		
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr), cout.tie(nullptr);
 
-	for (int i = 0; i < 15; i++)
+	cin >> t;
+
+	for (int i = 1; i <= 15; i++)
 	{
-		arr[0][i] = i + 1;
+		arr[0][i] = i;
 	}
 
 	for (int i = 0; i < t; i++)
