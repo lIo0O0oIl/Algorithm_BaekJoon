8c6e11d7384d6a6aa49eb9430fdbe997b75d2106
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2024-04-07 22:34:15 +0900

    수 정렬하기 2 풂

---
+++ BaekJoon/종이자르기.ing 2628.cpp
@@ -0,0 +1,14 @@
+// https://www.acmicpc.net/problem/2628
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int row, col;
+	int n;
+	cin >> col >> row >> n;
+	for (int i = 0; i < n; i++)		// 자르는 것의 수
+	{
+
+	}
+}
