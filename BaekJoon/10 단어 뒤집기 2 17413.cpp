e8a7377a842da9fa99a471c2ca30e964abc62fbf
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2024-04-10 23:17:00 +0900

    풂

---
+++ BaekJoon/10 단어 뒤집기 2 17413.cpp
@@ -0,0 +1,78 @@
+// https://www.acmicpc.net/problem/17413
+
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr), cout.tie(nullptr);
+
+	string input;
+	stack<char> s;
+	bool is_arrow = false;
+	while (cin >> input)
+	{
+		for (int i = 0; i < input.length(); i++)
+		{
+			if (input[i] == '<')
+			{
+				while (!s.empty())
+				{
+					cout << s.top();
+					s.pop();
+				}
+				is_arrow = true;
+			}
+			if (input[i] == '>')
+			{
+				is_arrow = false;
+				cout << input[i];
+				continue;
+			}
+
+			if (is_arrow)
+			{
+				cout << input[i];
+			}
+			else
+			{
+				s.push(input[i]);
+			}
+		}
+		if (is_arrow == false)
+		{
+			while (!s.empty())
+			{
+				cout << s.top();
+				s.pop();
+			}
+		}
+		cout << " ";
+	}
+}
+
+
+/*
+baekjoon online judge
+noojkeab enilno egduj
+ 
+<ab cd>ef gh<ij kl>
+<ab cd>fe hg<ij kl>
+
+<ab cd>ef gh<ij kl>
+<ab cd>fe hg<ij kl>
+
+one1 two2 three3 4fourr 5five 6six
+1eno 2owt 3eerht rruof4 evif5 xis6
+
+<int><max>2147483647<long long><max>9223372036854775807
+<int><max>7463847412<long long><max>7085774586302733229
+
+<problem>17413<is hardest>problem ever<end>
+<problem>31471<is hardest>melborp reve<end>
+
+<   space   >space space space<    spa   c e>
+<   space   >ecaps ecaps ecaps<    spa   c e>
+*/
---
+++ BaekJoon/dfsf.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << 2024 << endl;
+	cout << 04 << endl;
+	cout << 16 << endl;
+}
