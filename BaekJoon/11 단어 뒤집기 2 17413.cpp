5ac9480041208ee62535a3890b2efb0eb7f0bd60
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2024-04-11 17:37:18 +0900

    오늘의 날짜는, 단어 뒤집기 2 풂

--- BaekJoon/11 단어 뒤집기 2 17413.cpp
+++ BaekJoon/11 단어 뒤집기 2 17413.cpp
@@ -1,3 +1,5 @@
+// https://www.acmicpc.net/problem/17413
+
 #include <iostream>
 #include <string>
 #include <stack>
@@ -37,6 +39,8 @@ int main() {
 				cout << s.top();
 				s.pop();
 			}
+			cout << input[i];
+			continue;
 		}
 
 		if (is_arrow)
@@ -54,3 +58,24 @@ int main() {
 		s.pop();
 	}
 }
+
+
+/*
+baekjoon online judge
+noojkeab enilno egduj
+ 
+<ab cd>ef gh<ij kl>
+<ab cd>fe hg<ij kl>
+
+one1 two2 three3 4fourr 5five 6six
+1eno 2owt 3eerht rruof4 evif5 xis6
+
+<int><max>2147483647<long long><max>9223372036854775807
+<int><max>7463847412<long long><max>7085774586302733229
+
+<problem>17413<is hardest>problem ever<end>
+<problem>31471<is hardest>melborp reve<end>
+
+<   space   >space space space<    spa   c e>
+<   space   >ecaps ecaps ecaps<    spa   c e>
+*/
--- BaekJoon/11 오늘의 날짜는 16170.cpp
+++ BaekJoon/11 오늘의 날짜는 16170.cpp
@@ -1,8 +1,10 @@
+// https://www.acmicpc.net/problem/16170
+
 #include <iostream>
 using namespace std;
 
 int main() {
 	cout << 2024 << endl;
 	cout << 04 << endl;
-	cout << 16 << endl;
+	cout << 11 << endl;
 }
