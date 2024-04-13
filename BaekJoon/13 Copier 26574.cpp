e6b02ad2f4de66463d348ae936a4c8db382acf7c
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2024-04-13 20:39:25 +0900

    Copier 풂

---
+++ BaekJoon/13 Copier 26574.cpp
@@ -0,0 +1,15 @@
+// https://www.acmicpc.net/problem/26574
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n;
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> m;
+        cout << m << " " << m << '\n';
+    }
+}
