b309233a8e130fb1b416e0a2f8e49214abada531
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2024-04-14 20:14:20 +0900

    히스토그램 풂

---
+++ BaekJoon/14 히스토그램 13752.cpp
@@ -0,0 +1,18 @@
+// https://www.acmicpc.net/problem/13752
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t, n;
+	cin >> t;
+	for (int i = 0; i < t; i++)
+	{
+		cin >> n;
+		for (int j = 0; j < n; j++)
+		{
+			cout << "=";
+		}
+		cout << endl;
+	}
+}
