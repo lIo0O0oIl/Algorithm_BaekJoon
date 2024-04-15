62e916d098c2300d4b26f2532a51166775dbbf56
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2024-04-15 14:10:21 +0900

    나는 행복합니다~ 풂

---
+++ BaekJoon/15 나는 행복합니다~ 14652.cpp
@@ -0,0 +1,10 @@
+// https://www.acmicpc.net/problem/14652
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m, k;
+    cin >> n >> m >> k;
+    cout << k / m << " " << k % m;
+}
