d8774e0f58eb47234d27760e5390f1773d17e57e
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2024-04-24 23:08:58 +0900

    A+B - 6 풂

---
+++ BaekJoon/24 A+B - 6 10953.cpp
@@ -0,0 +1,17 @@
+// https://www.acmicpc.net/problem/10953
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m, t;
+    char c;
+
+    cin >> t;
+
+    for (int i = 0; i < t; i++)
+    {
+        cin >> n >> c >> m;
+        cout << n + m << endl;
+    }
+}
