8a1936c22f8fb84f8286d6cfcba9c1580ee43802
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2024-05-01 11:50:49 +0900

    최대공약수와 최소공배수 풂

--- BaekJoon/1 최대공약수와 최소공배수 2609.cpp
+++ BaekJoon/1 최대공약수와 최소공배수 2609.cpp
@@ -4,16 +4,16 @@
 using namespace std;
 
 int main() {
-	int n, m, common = 0, multiple = 0;
+	int n, m, common = 1;
 	cin >> n >> m;
 	int big = n > m ? n : m;
 	for (int i = big; i >= 2; --i)
 	{
-		if (n % i == 0 && m % i == 0)		// 큰거부터 내려가다가 
+		if (n % i == 0 && m % i == 0)
 		{
 			common = i;
 			break;
 		}
 	}
-	cout << common << '\n' << multiple;
+	cout << common << '\n' << n * m / common;
 }
