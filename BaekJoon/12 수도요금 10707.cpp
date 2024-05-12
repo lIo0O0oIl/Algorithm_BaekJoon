ac02483f195f535cd739b31f3b56d617ef30ccb7
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2024-05-12 22:50:01 +0900

    수도요금 풂

---
+++ BaekJoon/12 수도요금 10707.cpp
@@ -0,0 +1,23 @@
+//https://www.acmicpc.net/problem/10707
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c, d, p;
+    int x, y;
+
+    cin >> a >> b >> c >> d >> p;
+
+    x = a * p;
+
+    if (p > c)
+        y = (p - c) * d + b;
+    else
+        y = b;
+
+    if (x < y)
+        cout << x;
+    else
+        cout << y;
+}
