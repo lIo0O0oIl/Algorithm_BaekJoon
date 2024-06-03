e7282166407fccdd049ccc45562295c6d2760155
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2024-06-03 22:08:53 +0900

    Celebrity jeopardy 퓲

---
+++ BaekJoon/3 Celebrity jeopardy 3765.cpp
@@ -0,0 +1,15 @@
+// https://www.acmicpc.net/problem/3765
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string n;
+
+    while (getline(cin, n))
+    {
+        cout << n << '\n';
+    }
+}
