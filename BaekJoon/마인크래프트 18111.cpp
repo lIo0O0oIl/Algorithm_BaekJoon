7828f57727c90e584aaab11e89051f4436a03651
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2024-06-04 09:03:07 +0900

    좌표 정렬하기 2 풂

--- BaekJoon/마인크래프트 18111.cpp
+++ BaekJoon/마인크래프트 18111.cpp
@@ -85,10 +85,15 @@ int main()
 	int n, m;
 	long long b;
 	int land[501][501];
-	float avg = 0;		// 평균값
+	int avg = 0;		// 평균값
 	int time = 0;
+
+	int bb, need;
+	bb = need = 0;
+
 	cin >> n >> m >> b;
 	avg += b;
+
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < m; j++)
@@ -98,9 +103,41 @@ int main()
 		}
 	}
 
-	avg /= n * m;
+	avg = round(avg / (n * m));		// 평균값 구하기
 
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			if (land[i][j] != avg)		// 돌다가 평균이 아니면
+			{
+				if (land[i][j] > avg)		// 평균보다 크면
+				{
+					time = 2 * (land[i][j] - avg);		// 시간 더해주고
+					bb += land[i][j] - avg;		// 쓸 수 있는 블럭 추가
+				}
+				else		// 평균보다 작으면
+				{
+					need += avg - land[i][j];		// 필요한 블럭들 더해주기
+				}
+			}
+		}
+	}
 
+	if (need != 0)		// 필요한 블럭이 있다면
+	{
+		if (need < b)		// 가지고 있는 것이 더 크면
+		{
+			time += need;
+		}
+		else if (need < b + bb)
+		{
+			time += bb * 2;
+			time += need;
+		}
+	}
+
+	cout << time << " " << round(avg);
 }
 
 
@@ -108,9 +145,6 @@ int main()
 1. 배열을 입력 받는다.
 2. 평균을 구한다. 구해서 뭐해 int 로 해서 가장 가까운 것으로 한다.??
 3. 시간이 있다?
-
-
-모르겠고 일단 첫번째 반례는 통과함. 블럭을 설치할 떄를 생각해서 또 코드 짜주기
 
 
 내가 있는 블럭까지 해서 평균을 구해볼까?
