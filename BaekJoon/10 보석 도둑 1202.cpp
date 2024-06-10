7becd42e7ae18610e2c40eb39c9d06221aa22db4
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2024-06-10 19:38:01 +0900

    보석 도둑 풂

---
+++ BaekJoon/10 보석 도둑 1202.cpp
@@ -0,0 +1,101 @@
+// https://www.acmicpc.net/problem/1202
+
+#include <iostream>
+#include <set>
+#include <algorithm>
+using namespace std;
+
+struct Item
+{
+	long long m, v;		// 무게, 가치
+};
+
+bool Compare(Item one, Item two)		// 가치있고 무거운 순으로 정렬
+{
+	if (one.v != two.v) return one.v > two.v;
+	else return one.m > two.m;
+}
+
+Item item[300010];		// 보석의 갯수
+multiset<long long> bags;
+bool use[300010];		// 가방에 보석이 들어가 있는가
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr); cout.tie(nullptr);
+
+	int n, k;
+	double m, v;
+	long long input, total = 0;
+
+	cin >> n >> k;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> item[i].m >> item[i].v;
+	}
+	sort(item, item + n, Compare);
+
+	for (int i = 0; i < k; i++)
+	{
+		cin >> input;		// 가방의 최대 무게 얻어오기
+		bags.insert(input);
+	}
+
+	int index = 0;		// 보석 가장 큰거
+
+	for (int i = 0; i < n; i++)		// 보석을 돌리면서 
+	{
+		if (bags.lower_bound(item[index].m) == bags.end())		// 무게가 넣을 수 있는게 없으면
+		{
+			index++;
+			continue;
+		}
+
+		total += item[index].v;
+
+		bags.erase(bags.lower_bound(item[index].m));
+		index++;
+	}
+
+	cout << total;
+}
+
+
+/*
+입력 :
+3 2
+1 65
+5 23
+2 99
+10
+2
+
+정렬 :
+2 99
+1 65
+5 23
+
+넣어진 것 :
+10 - 2 99
+2  - 1 65
+
+답 :
+164
+
+
+4 4
+2 1
+2 2
+2 3
+2 4
+1
+1
+2
+2
+= 7
+
+
+https://www.acmicpc.net/board/view/107919
+https://www.acmicpc.net/board/view/95687
+
+*/
