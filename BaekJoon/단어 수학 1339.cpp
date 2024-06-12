8e03f86c52534ccfac86b3ff8cddb96cc2084691
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2024-06-12 11:46:03 +0900

    Olympiad Pizza 풂

--- BaekJoon/단어 수학 1339.cpp
+++ BaekJoon/단어 수학 1339.cpp
@@ -1,58 +1,69 @@
 // https://www.acmicpc.net/problem/1339
 
 #include <iostream>
+#include <algorithm>
 #include <string>
 #include <vector>
 #include <unordered_map>
 using namespace std;
 
+bool com(string a, string b)
+{
+	return a > b;
+}
+
 int main()
 {
-    int n, now = 9;
-    vector<string> vec;
-    unordered_map<string, int> map;
-    string input, temp = "";
-    long long result  = 0;
+	int n, now = 9, nowIndex = 8;
+	vector<string> vec;
+	unordered_map<char, int> map = { {'0', 0} };
+	string input, temp = "000000000";
+	long long result = 0;
 
-    cin >> n;
-    for (int i = 0; i < n; i++)
-    {
-        cin >> input;
-        vec.push_back(input);
-    }
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> input;
+		for (int i = input.size() - 1; i >= 0; --i)
+		{
+			temp[nowIndex] = input[i];
+			nowIndex--;
+		}
+		vec.push_back(temp);
+		temp = "000000000";
+		nowIndex = 8;
+	}
+	sort(vec.begin(), vec.end(), com);
 
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            if (vec[j].size() >= i)     // 지금 인덱스보다 크고
-            {
-                char ch[1];
-                ch[0] = vec[j][i];
-                string temp(ch);
-                if (map.find(temp) == map.end())       // 이게 지금 들어있지 않으면
-                {
-                    map.insert({ temp, now });
-                    now--;
-                }
-            }
-        }
-    }
+	for (int i = 0; i < 9; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			if (map.find(vec[j][i]) == map.end())       // 이게 지금 들어있지 않으면
+			{
+				map.insert({ vec[j][i], now });
+				now--;
+			}
+		}
+	}
 
-    for (auto i : map)
-    {
-        cout << i.first << " " << i.second << endl;
-    }
+	//for (auto i : map)
+	//{
+	//	cout << i.first << " " << i.second << endl;
+	//}
 
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = vec[i].size() - 1; j >= 0; --j)
-        {
-            temp += map[vec[i][j]];
-        }
-        result += stoll(temp);
-    }
-    cout << result;
+	for (int i = 0; i < n; i++)
+		{
+			for (int j = 8; j >= 0; --j)
+			{
+				temp[nowIndex] = map[vec[i][j]] + '0';
+				nowIndex--;
+			}
+			result += stoll(temp);
+			temp = "000000000";
+			nowIndex = 8;
+		}
+	cout << result;
 }
 
 
@@ -62,8 +73,8 @@ aaa
 1998
 
 
-ACDEB
-     GCF
+ACDEB       = 5
+    GCF  = 3
 a = 9
 c = 8
 g = 7
