a5746a8c800568f880e5b1ec8594da08a6f683e3
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2024-06-13 20:15:22 +0900

    사과 풂

---
+++ BaekJoon/13 사과 10833.cpp
@@ -0,0 +1,17 @@
+// https://www.acmicpc.net/problem/10833
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, s, a, sum = 0;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> s >> a;
+        sum += a % s;
+    }
+
+    cout << sum;
+}
