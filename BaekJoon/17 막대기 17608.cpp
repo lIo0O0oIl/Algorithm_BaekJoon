7cb36b1a924e626df61850f2cd816992969dfcde
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2024-06-17 18:17:30 +0900

    막대기 풂

---
+++ BaekJoon/17 막대기 17608.cpp
@@ -0,0 +1,29 @@
+// https://www.acmicpc.net/problem/17608
+
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main()
+{
+	int n, input, top, count = 1;
+	stack<int> s;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> input;
+		s.push(input);
+	}
+	top = s.top();
+	s.pop();
+	while (!s.empty())
+	{
+		if (s.top() > top)
+		{
+			count++;
+			top = s.top();
+		}
+		s.pop();
+	}
+	cout << count;
+}
