9385359763f40fdd5580c0478ef2f52d463f6ab9
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2024-06-27 08:45:26 +0900

    대표값 풂

---
+++ BaekJoon/27 대표값2 2587.cpp
@@ -0,0 +1,17 @@
+// https://www.acmicpc.net/problem/2587
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	int arr[6], total = 0;
+	for (int i = 0; i < 5; i++)
+	{
+		cin >> arr[i];
+		total += arr[i];
+	}
+	sort(arr, arr + 5);
+	cout << total / 5 << '\n' << arr[2];
+}
