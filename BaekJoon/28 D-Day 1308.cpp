aebf3267940b9f522f9afaa3dd7f902740a44358
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2024-06-28 16:40:02 +0900

    적어도 대부분의 배수 풂

---
+++ BaekJoon/28 D-Day 1308.cpp
@@ -0,0 +1,14 @@
+// https://www.acmicpc.net/problem/1308
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int startYear, startMonth, startDay;
+	int endYear, endMonth, endDay;
+	cin >> startYear >> startMonth >> startDay;
+	cin >> endYear >> endMonth >> endDay;
+
+
+}
