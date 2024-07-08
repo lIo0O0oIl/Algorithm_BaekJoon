b7bd5e6aad3a4c2d3bc3310e348aaf1f696601bf
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2024-07-08 15:18:09 +0900

    과민성 대장 증후군 풂

---
+++ BaekJoon/8 과민성 대장 증후군 31831.cpp
@@ -0,0 +1,25 @@
+// https://www.acmicpc.net/problem/31831
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, m, k, sum, cnt;
+    sum = cnt = 0;
+    cin >> n >> m;
+
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> k;
+        if (sum + k > 0)
+            sum += k;
+        else
+            sum = 0;
+
+        if (sum >= m)
+            cnt++;
+    }
+
+    cout << cnt;
+}
