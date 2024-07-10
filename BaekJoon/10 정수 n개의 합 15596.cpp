f097cd678f35b600bb3c70170cc802dbc67e7ff1
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2024-07-10 23:06:40 +0900

    정수 n개의 합 풂

---
+++ BaekJoon/10 정수 n개의 합 15596.cpp
@@ -0,0 +1,13 @@
+// https://www.acmicpc.net/problem/15596
+
+#include <vector>
+using namespace std;
+
+long long sum(vector<int>& v) {
+    long long result = 0;
+
+    for (int i = 0; i < v.size(); i++) {
+        result += v[i];
+    }
+    return result;
+}
