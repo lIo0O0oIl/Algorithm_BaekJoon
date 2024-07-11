f63a007609aee5f663b3d12e5e34c04524170cf2
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2024-07-11 23:16:23 +0900

    사파리월드 풂

---
+++ BaekJoon/11 사파리월드 2420.cpp
@@ -0,0 +1,12 @@
+// https://www.acmicpc.net/problem/2420
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+    long long n, m;
+    cin >> n >> m;
+    cout << abs(n - m);
+}
