8b96b6bb38a4fea299ad66ad40cd1c83686ab5a5
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2024-07-14 22:58:02 +0900

    체육은 수학과목 입니다 풂

---
+++ BaekJoon/14 체육은 수학과목 입니다 32025.cpp
@@ -0,0 +1,15 @@
+// https://www.acmicpc.net/problem/32025
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+
+    if (n >= m)
+        cout << (m * 100) / 2;
+    else
+        cout << (n * 100) / 2;
+}
