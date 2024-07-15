496b790e55dafbbf26ffc87e9d85020381248116
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2024-07-15 09:25:58 +0900

    첫 글자를 대문자로 풂

---
+++ BaekJoon/15 첫 글자를 대문자로 4458.cpp
@@ -0,0 +1,23 @@
+// https://www.acmicpc.net/problem/4458
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int n;
+	string input;
+	cin >> n;
+	cin.ignore();
+
+	for (int i = 0; i < n; i++)
+	{
+		getline(cin, input);
+		if ('a' <= input[0] && 'z' >= input[0])
+		{
+			input[0] -= 32;
+		}
+		cout << input << '\n';
+	}
+}
