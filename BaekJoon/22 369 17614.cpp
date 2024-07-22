5da552c168d441b6170dc566c1330dcee1f7454c
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2024-07-22 22:09:09 +0900

    369 풂

---
+++ BaekJoon/22 369 17614.cpp
@@ -0,0 +1,21 @@
+// https://www.acmicpc.net/problem/17614
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int n, result = 0;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        string s = to_string(i);
+
+        for (auto a : s) {
+            if (a == '3' || a == '6' || a == '9')
+                result++;
+        }
+    }
+
+    cout << result;
+}
