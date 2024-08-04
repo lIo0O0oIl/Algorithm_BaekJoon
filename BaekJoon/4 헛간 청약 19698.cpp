26a38044dd539c0e17ccda42a95daf7375a8c054
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2024-08-04 23:16:54 +0900

    헛간 청약 풂

---
+++ BaekJoon/4 헛간 청약 19698.cpp
@@ -0,0 +1,15 @@
+// https://www.acmicpc.net/problem/19698
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, w, h, l;
+    cin >> n >> w >> h >> l;
+
+    if (n <= (w / l) * (h / l))
+        cout << n;
+    else
+        cout << (w / l) * (h / l);
+}
