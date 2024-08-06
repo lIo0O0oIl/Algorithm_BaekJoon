3bd1ff9d1c469fd29f89fb893a8773af1adcf4e4
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2024-08-06 11:18:52 +0900

    임스의 메이플컵 풂

---
+++ BaekJoon/6 임스의 메이플컵 29790.cpp
@@ -0,0 +1,20 @@
+// https://www.acmicpc.net/problem/29790
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, u, l;
+    cin >> n >> u >> l;
+
+    if (n >= 1000) {
+        if (u >= 8000 || l >= 260)
+            cout << "Very Good" << endl;
+        else
+            cout << "Good" << endl;
+    }
+    else
+        cout << "Bad" << endl;
+
+    return 0;
+}
