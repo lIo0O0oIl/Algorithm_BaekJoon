798308f939294d66b6d8db9ce892cb34bd8d958e
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2024-08-08 14:27:19 +0900

    삼각현과 세 변 풂

---
+++ BaekJoon/8 삼각형과 세 변 5073.cpp
@@ -0,0 +1,24 @@
+// https://www.acmicpc.net/problem/5073
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	int a, b, c;
+	while (true)
+	{
+		cin >> a >> b >> c;
+		if (a == 0 && b == 0 && c == 0) break;
+
+		if (a + b + c - max({ a, b, c }) > max({ a, b, c }))
+		{
+			if (a == b && b == c && c == a) cout << "Equilateral";
+			else if (a == b || b == c || c == a) cout << "Isosceles";
+			else cout << "Scalene";
+		}
+		else cout << "Invalid";
+		cout << '\n';
+	}
+}
