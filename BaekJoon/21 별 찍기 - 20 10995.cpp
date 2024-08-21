2df8f79fb3ec3b1f0d5b9ce265b88e43109687e1
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2024-08-21 09:26:18 +0900

    별 찍기 - 20 풂

---
+++ BaekJoon/21 별 찍기 - 20 10995.cpp
@@ -0,0 +1,22 @@
+// https://www.acmicpc.net/problem/10995
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (i % 2 == 1)
+            cout << " ";
+
+        for (int j = 0; j < n; j++)
+            cout << "* ";
+
+        cout << endl;
+    }
+}
