e31f1dbeb00698873c7b0b26296da7d2000506f1
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2024-09-01 18:33:58 +0900

    Hunt The Rabbit 풂

---
+++ BaekJoon/1 Hunt The Rabbit 13777.cpp
@@ -0,0 +1,31 @@
+// https://www.acmicpc.net/problem/13777
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int inputNum;
+	while (true)
+	{
+		cin >> inputNum;
+		if (inputNum == 0) break;
+
+		int left = 1, right = 50;
+		while (true)
+		{
+			int mid = (left + right) / 2;
+			cout << mid << ' ';
+			if (mid == inputNum) break;
+			else if (inputNum > mid)
+			{
+				left = mid + 1;
+			}
+			else
+			{
+				right = mid - 1;
+			}
+		}
+		cout << '\n';
+	}
+}
