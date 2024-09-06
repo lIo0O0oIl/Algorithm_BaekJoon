54e1938847e5354c4933baa7822166e62df6b075
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2024-09-06 20:37:01 +0900

    A+B - 10 (제1편) 풂

---
+++ BaekJoon/6 A+B - 10 (제1편) 30917.cpp
@@ -0,0 +1,36 @@
+// https://www.acmicpc.net/problem/30917
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    for (int i = 1; i <= 9; ++i)
+    {
+        cout << "? A " << i << endl;
+        cin >> a;
+
+        if (a == 1)
+        {
+            a = i;
+
+            for (int i = 1; i <= 9; ++i)
+            {
+                cout << "? B " << i << endl;
+                cin >> b;
+
+                if (b == 1)
+                {
+                    b = i;
+                    break;
+                }
+            }
+
+            break;
+        }
+    }
+
+    cout << "! " << (a + b);
+}
