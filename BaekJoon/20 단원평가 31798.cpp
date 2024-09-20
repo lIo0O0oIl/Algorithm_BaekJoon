5cbda072435f6af10b5fd004621d9245b4467803
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2024-09-20 12:32:15 +0900

    단원평가 풂

---
+++ BaekJoon/20 단원평가 31798.cpp
@@ -0,0 +1,23 @@
+// https://www.acmicpc.net/problem/31798
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    if (a == 0)
+    {
+        cout << c * c - b;
+    }
+    else if (b == 0)
+    {
+        cout << c * c - a;
+    }
+    else if (c == 0)
+    {
+        cout << sqrt(a + b);
+    }
+}
