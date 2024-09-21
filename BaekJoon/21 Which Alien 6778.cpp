ee0c64177b5f8d3457f65cfb296cdd33d8be8751
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2024-09-21 21:17:51 +0900

    Which Alien 풂

---
+++ BaekJoon/21 Which Alien 6778.cpp
@@ -0,0 +1,16 @@
+// https://www.acmicpc.net/problem/6778
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, eye;
+    cin >> n >> eye;
+
+    if (n >= 3 && eye <= 4)
+        cout << "TroyMartian\n";
+    if (n <= 6 && eye >= 2)
+        cout << "VladSaturnian\n";
+    if (n <= 2 && eye <= 3)
+        cout << "GraemeMercurian\n";
+}
