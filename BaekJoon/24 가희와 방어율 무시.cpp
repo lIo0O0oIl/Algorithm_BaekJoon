cd59ff90172f46df4fee11f7d128024e0bbcc18d
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2024-09-24 18:14:16 +0900

    가희와 방어율 무시 풂

---
+++ BaekJoon/24 가희와 방어율 무시.cpp
@@ -0,0 +1,16 @@
+// https://www.acmicpc.net/problem/25238
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    double percent = n - n * (m / 100.0);
+
+    if (percent < 100)
+        cout << 1;
+    else
+        cout << 0;
+}
