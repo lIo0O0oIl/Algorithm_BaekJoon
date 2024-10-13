5df9b64617bf30bc9644ee42a27f4d6149fc9d9a
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2024-10-13 23:52:33 +0900

    바코드 닉네임 풂

---
+++ BaekJoon/13 바코드 닉네임 32288.cpp
@@ -0,0 +1,28 @@
+// https://www.acmicpc.net/problem/32288
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int n;
+    string str;
+
+    cin >> n;
+    cin >> str;
+
+    for (int i = 0; i < n; ++i)
+    {
+        if (str[i] == 'I')
+        {
+            str[i] = tolower(str[i]);
+            cout << str[i];
+        }
+        else
+        {
+            str[i] = towupper(str[i]);
+            cout << str[i];
+        }
+    }
+}
