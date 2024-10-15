b738ab577051427407d406748ed59ffa32faf6bf
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2024-10-15 23:08:10 +0900

    Identifying tea 풂

---
+++ BaekJoon/15 Identifying tea 11549.cpp
@@ -0,0 +1,18 @@
+// https://www.acmicpc.net/problem/11549
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, cnt = 0, x;
+
+    cin >> n;
+
+    for (int i = 0; i < 5; i++) {
+        cin >> x;
+        if (x == n)
+            cnt++;
+    }
+
+    cout << cnt << endl;
+}
