56d484272d8c443cdbf8a8ba29a1af0e601caf01
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2024-10-17 10:08:03 +0900

    별 찍기 - 23 풂

--- BaekJoon/17 별 찍기 - 23 13015.cpp
+++ BaekJoon/17 별 찍기 - 23 13015.cpp
@@ -11,14 +11,80 @@ int main()
 	int n;
 	cin >> n;
 
-	for (int i = 0; i < n; i++)
-	{
+	// À­ºÎºÐ Ãâ·Â
+	for (int i = 0; i < n; i++) cout << '*';
+	for (int i = 0; i < n * 2 - 3; i++) cout << ' ';
+	for (int i = 0; i < n; i++) cout << '*';
+	cout << '\n';
 
-	}
 	for (int i = 0; i < n - 1; i++)
 	{
-
+		for (int j = 0; j < i + 1; j++) cout << ' ';
+		cout << '*';
+		for (int j = 0; j < n - 2; j++) cout << ' ';
+		cout << '*';
+		if (i != n - 2)
+		{
+			for (int j = 0; j < ((n - 2) * 2) - (i * 2) - 1; j++) cout << ' ';
+			cout << '*';
+		}
+		for (int j = 0; j < n - 2; j++) cout << ' ';
+		cout << '*' << '\n';
 	}
 
+	for (int i = 0; i < n - 2; i++)
+	{
+		for (int j = 0; j < n - 2 - i; j++) cout << ' ';
+		cout << '*';
+		for (int j = 0; j < n - 2; j++) cout << ' ';
+		cout << '*';
+		for (int j = 0; j < 1 + (i * 2); j++) cout << ' ';
+		cout << '*';
+		for (int j = 0; j < n - 2; j++) cout << ' ';
+		cout << '*' << '\n';
+	}
 
+	for (int i = 0; i < n; i++) cout << '*';
+	for (int i = 0; i < n * 2 - 3; i++) cout << ' ';
+	for (int i = 0; i < n; i++) cout << '*';
 }
+
+
+/*
+1Àº ¾È µé¾î¿È
+
+2
+** **
+ ***
+** **
+
+3
+***   ***
+  * * * *
+   * * *
+  * * * *
+***   ***
+
+4
+****     ****
+ *  *    *  *
+  *  * *  *
+   *  *  *
+  *  * *  *
+ *  *   *  *
+****     ****
+
+5
+*****       *****
+ *   *     *   *
+  *   *   *   *
+   *   * *   *
+	*   *   *
+   *   * *   *
+  *   *   *   *
+ *   *     *   *
+*****       *****
+
+
+
+*/
