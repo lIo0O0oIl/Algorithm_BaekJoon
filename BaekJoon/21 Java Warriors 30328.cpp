e8c58883c33c06193dd94dafdc59c1143b78c3fc
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2024-10-21 09:21:54 +0900

    이상한 기호, Java Warriors 풂

---
+++ BaekJoon/21 Java Warriors 30328.cpp
@@ -0,0 +1,11 @@
+// https://www.acmicpc.net/problem/30328
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	cout << n * 4000;
+}
---
+++ BaekJoon/21 이상한 기호 15964.cpp
@@ -0,0 +1,11 @@
+// https://www.acmicpc.net/problem/15964
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	long long a, b;
+	cin >> a >> b;
+	cout << (a + b) * (a - b);
+}
