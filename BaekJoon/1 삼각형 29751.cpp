abbef64c93fe6b5ce4000ad0d5d903149b9922ca
lIo0O0oIl <98943168+lIo0O0oIl@users.noreply.github.com>
2024-11-01 21:17:49 +0900

    삼각형 풂

---
+++ BaekJoon/1 삼각형 29751.cpp
@@ -0,0 +1,13 @@
+// https://www.acmicpc.net/problem/29751
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    cout << fixed;
+    cout.precision(1);
+    cout << n * m * 0.5f;
+}
