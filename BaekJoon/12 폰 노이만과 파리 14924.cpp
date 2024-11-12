c43262e5a9d36e772b79f55abe2a6516477ec21d
lIo0O0oIl <98943168+lIo0O0oIl@users.noreply.github.com>
2024-11-12 23:15:56 +0900

    폰 노이만과 파리 풂

---
+++ BaekJoon/12 폰 노이만과 파리 14924.cpp
@@ -0,0 +1,12 @@
+// https://www.acmicpc.net/problem/14924
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int s, t, d;
+    cin >> s >> t >> d;
+
+    cout << (d / (s * 2)) * t;
+}
