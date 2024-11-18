fbe051ceb70f3c04e1241d529f209499a9e50a7f
lIo0O0oIl <98943168+lIo0O0oIl@users.noreply.github.com>
2024-11-18 22:55:04 +0900

    SUAPC 2024 Winter 풂

---
+++ BaekJoon/18 SUAPC 2024 Winter 32154.cpp
@@ -0,0 +1,54 @@
+// https://www.acmicpc.net/problem/32154
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    switch (n)
+    {
+    case 1:
+        cout << "11" << endl;
+        cout << "A B C D E F G H J L M";
+        break;
+    case 2:
+        cout << "9" << endl;
+        cout << "A C E F G H I L M";
+        break;
+    case 3:
+        cout << "9" << endl;
+        cout << "A C E F G H I L M";
+        break;
+    case 4:
+        cout << "9" << endl;
+        cout << "A B C E F G H L M";
+        break;
+    case 5:
+        cout << "8" << endl;
+        cout << "A C E F G H L M";
+        break;
+    case 6:
+        cout << "8" << endl;
+        cout << "A C E F G H L M";
+        break;
+    case 7:
+        cout << "8" << endl;
+        cout << "A C E F G H L M";
+        break;
+    case 8:
+        cout << "8" << endl;
+        cout << "A C E F G H L M";
+        break;
+    case 9:
+        cout << "8" << endl;
+        cout << "A C E F G H L M";
+        break;
+    case 10:
+        cout << "8" << endl;
+        cout << "A B C F G H L M";
+        break;
+    }
+}
