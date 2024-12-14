5e08277ff9dd4ed9fd0f0db37734d60b3898929f
lIo0O0oIl <98943168+lIo0O0oIl@users.noreply.github.com>
2024-12-14 23:14:35 +0900

    三方比較 (Three-Way Comparison) 풂

---
+++ BaekJoon/14 三方比較Three-Way Comparison 27328.cpp
@@ -0,0 +1,16 @@
+// 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    if (n == m)
+        cout << 0;
+    else if (n > m)
+        cout << 1;
+    else
+        cout << -1;
+}
