cc91501643af3a542042f0c812bd1d0a0ed67982
lIo0O0oIl <98943168+lIo0O0oIl@users.noreply.github.com>
2024-12-16 11:20:38 +0900

    입국심사 풂

---
+++ BaekJoon/16 입국심사 3079.cpp
@@ -0,0 +1,42 @@
+// https://www.acmicpc.net/problem/3079
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using ll = long long;
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr), cout.tie(nullptr);
+
+	ll n, m, temp;		// 입국심사대 개수, 사람 수
+	cin >> n >> m;
+	vector<ll> times(n);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> times[i];
+	}
+	sort(times.begin(), times.end());
+	ll left = times.front();
+	ll right = times.back() * m;
+	ll people, mid = 0, answer = 0;
+	while (left <= right)
+	{
+		people = 0;
+		mid = (left + right) / 2;
+		for (auto t : times)
+		{
+			people += mid / t;
+			if (people > m) break;		// 사람수를 크게 넘지마
+		}
+		if (people >= m)
+		{
+			answer = mid;
+			right = mid - 1;
+		}
+		else left = mid + 1;
+	}
+	cout << answer;
+}
