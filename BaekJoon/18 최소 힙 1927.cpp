9ffd1324557b783d7614388a0cff9ea8c57b4d00
lIo0O0oIl <98943168+lIo0O0oIl@users.noreply.github.com>
2024-12-18 09:43:28 +0900

    최소 힙 풂

---
+++ BaekJoon/18 최소 힙 1927.cpp
@@ -0,0 +1,29 @@
+// https://www.acmicpc.net/problem/1927
+
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr), cout.tie(nullptr);
+
+	long long n, input;
+	priority_queue<long long, vector<long long>, greater<long long>> q;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> input;
+		if (input != 0) q.push(input);
+		else
+		{
+			if (q.empty()) cout << 0 << '\n';
+			else
+			{
+				cout << q.top() << '\n';
+				q.pop();
+			}
+		}
+	}
+}
