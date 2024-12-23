1529811549111eea1df13f55c05c8aaf3e2be470
lIo0O0oIl <98943168+lIo0O0oIl@users.noreply.github.com>
2024-12-23 12:13:44 +0900

    가장 쉬운 문제를 찾는 문제 풂

---
+++ BaekJoon/23 가장 쉬운 문제를 찾는 문제 22966.cpp
@@ -0,0 +1,23 @@
+// https://www.acmicpc.net/problem/22966
+
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main()
+{
+	map<int, string> map;
+	int n, level;
+	string name;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> name >> level;
+		map[level] = name;
+	}
+	for (auto& name : map)
+	{
+		cout << name.second;
+		break;
+	}
+}
