027c5ee7c4690b238b897a7372eed86ab2938785
lIo0O0oIl <98943168+lIo0O0oIl@users.noreply.github.com>
2025-01-01 22:07:23 +0900

    아파트 풂

---
+++ BaekJoon/1 아파트 32979.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    int n, m;
+    queue<int> apt;
+    cin >> n >> m;
+    
+    for (int i = 0; i < n * 2; ++i)
+    {
+        int k;
+        cin >> k;
+        apt.push(k);
+    }
+    
+    for (int i = 0; i < m; ++i)
+    {
+        int k, temp;
+        cin >> k;
+        
+        for (int j = 0; j < k; ++j)
+        {
+            temp = apt.front();
+            if (j + 1 < k)
+            {
+                apt.pop();
+                apt.push(temp);
+            }
+        }
+        cout << temp << " ";
+    }
+}
+
+// 1월 1일에 풂
