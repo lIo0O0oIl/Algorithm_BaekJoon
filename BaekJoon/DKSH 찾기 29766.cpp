4b0817dcd64ac8de9ee4df84db534940798a872e
lIo0O0oIl <98943168+lIo0O0oIl@users.noreply.github.com>
2025-01-18 23:54:43 +0900

    LCS 풂

---
+++ BaekJoon/DKSH 찾기 29766.cpp
@@ -0,0 +1,16 @@
+// https://www.acmicpc.net/problem/29766
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string input;
+	cin >> input;
+
+	while (input.find("DKSH") != string::npos)
+	{
+		input.erase()
+	}
+}
