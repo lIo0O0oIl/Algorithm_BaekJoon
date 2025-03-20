640bc702f9a7d86b809789271365b11e22e7bc6e
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2025-03-20 22:18:58 +0900

    골뱅이 찍기 - 정사각형 풂

---
+++ BaekJoon/20 골뱅이 찍기 - 정사각형 23794.cpp
@@ -0,0 +1,19 @@
+// https://www.acmicpc.net/problem/23794
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	for (int i = 0; i < n + 2; i++) cout << '@';
+	cout << '\n';
+	for (int i = 0; i < n; i++)
+	{
+		cout << '@';
+		for (int j = 0; j < n; j++) cout << ' ';
+		cout << "@\n";
+	}
+	for (int i = 0; i < n + 2; i++) cout << '@';
+}
