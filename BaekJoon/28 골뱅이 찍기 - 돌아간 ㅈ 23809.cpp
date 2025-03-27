2a3647b8deb4d9bb391f7bde9ccf30973ed2a83e
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2025-03-28 08:30:49 +0900

    골뱅이 찍기 - 돌아간 ㅈ 풂

---
+++ BaekJoon/28 골뱅이 찍기 - 돌아간 ㅈ 23809.cpp
@@ -0,0 +1,43 @@
+// https://www.acmicpc.net/problem/23809
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++) cout << '@';
+		for (int j = 0; j < n * 3; j++) cout << ' ';
+		for (int j = 0; j < n; j++) cout << '@';
+		cout << '\n';
+	}
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++) cout << '@';
+		for (int j = 0; j < n * 2; j++) cout << ' ';
+		for (int j = 0; j < n; j++) cout << '@';
+		cout << '\n';
+	}
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n * 3; j++) cout << '@';
+		cout << '\n';
+	}
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++) cout << '@';
+		for (int j = 0; j < n * 2; j++) cout << ' ';
+		for (int j = 0; j < n; j++) cout << '@';
+		cout << '\n';
+	}
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++) cout << '@';
+		for (int j = 0; j < n * 3; j++) cout << ' ';
+		for (int j = 0; j < n; j++) cout << '@';
+		cout << '\n';
+	}
+}
