07cd86b1ced73c542e92a0e6d6ee440c4f9beda9
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2025-04-03 20:52:26 +0900

    아카라카 풂

---
+++ BaekJoon/3 아카라카 2 32652.cpp
@@ -0,0 +1,15 @@
+// https://www.acmicpc.net/problem/32652
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	cout << "AKA";
+	for (int i = 0; i < n; i++)
+	{
+		cout << "RAKA";
+	}
+}
