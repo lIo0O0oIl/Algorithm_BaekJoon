b1030a80e1e25d3362141ef40d5e6dd049986ae6
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2025-05-29 00:15:43 +0900

    영식이와 친구들 풂 ver.2

--- BaekJoon/28 영식이와 친구들 1592.cpp
+++ BaekJoon/28 영식이와 친구들 1592.cpp
@@ -1,72 +1,47 @@
 // https://www.acmicpc.net/problem/1592
 
 #include <iostream>
+#include <map>
 using namespace std;
 
-int arr[51];
-
-int main() {
-    int n, m, l;
-    cin >> n >> m >> l;
-
-    int i = 1;  
-    int cnt = -1;
-
-    while (true) {
-        arr[i]++;
-        cnt++;
-
-        if (arr[i] >= m) {
-            break;
-        }
-
-        if (arr[i] % 2 == 0) {
-            i -= l;
-            if (i <= 0) {
-                i += n;
-            }
-        }
-        else {
-            i = (i + l - 1) % n + 1;
-        }
-    }
-
-    cout << cnt << '\n';
+int main()
+{
+	int n, m, l, now = 1, answer = 0;
+	map<int, int> map;
+	cin >> n >> m >> l;
+	for (int i = 1; i <= n; i++)
+	{
+		map[i] = 1;
+	}
+	map[1] = 2;
+	while (true)
+	{
+		if (map[now] == m + 1)
+		{
+			break;
+		}
+		if (map[now] % 2 == 1)
+		{
+			now = (n + now) - l;
+			if (now <= 0) now = n - now;
+			if (now > n) now -= n;
+		}
+		else
+		{
+			now += l;
+			if (now > n) now -= n;
+		}
+		map[now]++;
+		answer++;
+	}
+	cout << answer;
 }
 
-//#include <iostream>
-//#include <map>
-//using namespace std;
-//
-//int main()
-//{
-//	int n, m, l, now = 1, answer = 1;
-//	map<int, int> map;
-//	cin >> n >> m >> l;
-//	for (int i = 1; i <= n; i++)
-//	{
-//		map[i] = 0;
-//	}
-//	map[1] = 1;
-//	while (true)
-//	{
-//		if (map[now] == m)
-//		{
-//			break;
-//		}
-//		if (map[now] % 2 == 1)
-//		{
-//			now += l;
-//			if (now > n) now -= n;
-//		}
-//		else
-//		{
-//			now -= l;
-//			if (now <= 0) now = n - now;
-//		}
-//
-//		map[now]++;
-//		answer++;
-//	}
-//	cout << answer;
-//}
+/*
+5 3 2
+1 3 5 2 4 1 4 2 5 3 1
+ 
+10 3 5
+1 6 1 6 1
+4
+*/
