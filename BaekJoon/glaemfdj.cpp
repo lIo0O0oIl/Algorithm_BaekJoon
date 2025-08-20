ee531dedb6a84d568c8ed9202bd148e3b73ee525
lIo0O0oIl <98943168+lIo0O0oIl@users.noreply.github.com>
2025-08-20 23:14:02 +0900

    problem? 풂

---
+++ BaekJoon/glaemfdj.cpp
@@ -0,0 +1,22 @@
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int k, h;
+    string input;
+    cin >> k;
+    for (int i = 0; i < k; ++i)
+    {
+        cin >> h >> input;
+        cout << "Data Set " << i + 1 << ":\n";
+        for (int j = 0; j < input.size(); ++j)
+        {
+            if (input[j] == 'c') h++;
+            else if (input[j] == 'b') h--;
+        }
+        cout << h << "\n\n";
+    }
+}
