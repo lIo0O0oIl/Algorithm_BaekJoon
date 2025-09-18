a476984c3d3442c676c3ec48155bb83064bf33b0
lIo0O0oIl <98943168+lIo0O0oIl@users.noreply.github.com>
2025-09-18 19:17:36 +0900

    Cupcake Party 풂

---
+++ BaekJoon/18 Cupcake Party  24568.cpp
@@ -0,0 +1,11 @@
+// https://www.acmicpc.net/problem/24568
+
+#include <iostream>
+using namespace std; 
+
+int main()
+{
+	int big, small;
+	cin >> big >> small;
+	Cout << big * 8 + small * 3 - 28;
+}
