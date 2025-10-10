f247ca20ab6c0c2cf42258a6d920c241d0775c94
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2025-10-10 18:23:50 +0900

    DORO 풂

---
+++ BaekJoon/10 DORO 34073.cpp
@@ -0,0 +1,16 @@
+// https://www.acmicpc.net/problem/34073
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	string input;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> input;
+		cout << input << "DORO ";
+	}
+}
