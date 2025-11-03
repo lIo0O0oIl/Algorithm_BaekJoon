c90c6c6851da27151d755332b92bfae966ccff6c
lIo0O0oIl <98943168+lIo0O0oIl@users.noreply.github.com>
2025-11-03 22:47:28 +0900

    Equality 풂

---
+++ BaekJoon/3 Equality 13985.cpp
@@ -0,0 +1,13 @@
+// https://www.acmicpc.net/problem/13985
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c;
+	char i, ii;
+	cin >> a >> i >> b >> ii >> c;
+	if (a + b == c) cout << "YES";
+	else cout << "NO";
+}
