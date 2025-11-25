78e466f976136c4036d823aa6285a1de2dfafe12
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2025-11-25 20:56:36 +0900

    Rats 풂

---
+++ BaekJoon/25 Rats 18301.cpp
@@ -0,0 +1,11 @@
+// https://www.acmicpc.net/problem/18301
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c;
+	cin >> a >> b >> c;
+	cout << (a + 1) * (b + 1) / (c + 1) - 1;
+}
