07d4292de7d270d1651d320c981c74244a8c7831
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2025-11-29 09:28:22 +0900

    정수의 개수 풂

---
+++ BaekJoon/29 정수의 개수 10821.cpp
@@ -0,0 +1,16 @@
+// https://www.acmicpc.net/problem/10821
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string input;
+	int answer = 0;
+	cin >> input;
+	for (int i = 0; i < input.size(); i++)
+	{
+		if (input[i] == ',') answer++;
+	}
+	cout << answer + 1;
+}
