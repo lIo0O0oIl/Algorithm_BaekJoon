83c007a3b9da154419789999363c58a3f2f0a6e4
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2025-12-12 08:11:34 +0900

    정열적인 정렬 풂

---
+++ BaekJoon/12 정열적인 정렬 16212.cpp
@@ -0,0 +1,25 @@
+// https://www.acmicpc.net/problem/16212
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr), cout.tie(nullptr);
+
+	int n;
+	cin >> n;
+	vector<int> vec(n);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> vec[i];
+	}
+	sort(vec.begin(), vec.end());
+	for (int i = 0; i < n; i++)
+	{
+		cout << vec[i] << ' ';
+	}
+}
