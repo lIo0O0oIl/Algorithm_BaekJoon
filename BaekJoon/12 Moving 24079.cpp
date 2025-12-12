533825e16bd563acc7420fffdc46f1edab4781d3
lIo0O0oIl <98943168+lIo0O0oIl@users.noreply.github.com>
2025-12-12 23:54:16 +0900

    Moving 풂

---
+++ BaekJoon/12 Moving 24079.cpp
@@ -0,0 +1,11 @@
+// https://www.acmicpc.net/problem/24079
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x, y, z;
+    cin >> x >> y >> z;
+    cout << (x + y > z ? 0 : 1);
+}
