b99a15b726081a9e33214a3de8541db4fd344dbd
lIo0O0oIl <98943168+lIo0O0oIl@users.noreply.github.com>
2025-12-25 08:51:45 +0900

    채플이 너무해 풂

---
+++ BaekJoon/25 채플이 너무해 34945.cpp
@@ -0,0 +1,11 @@
+// https://www.acmicpc.net/problem/34945
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x;
+    cin >> x;
+    cout << (x >= 6 ? "Success!" : "Oh My God!");
+}
