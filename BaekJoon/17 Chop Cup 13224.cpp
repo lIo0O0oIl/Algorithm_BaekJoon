791c88b2140a106fa6c97f4e4abebecdd5569efd
lIo0O0oIl <98943168+lIo0O0oIl@users.noreply.github.com>
2026-01-17 20:44:03 +0900

    Chop Cup 풂

---
+++ BaekJoon/17 Chop Cup 13224.cpp
@@ -0,0 +1,40 @@
+// https://www.acmicpc.net/problem/13224
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int arr[] = { 1, 0, 0 };
+	int temp;
+	string input;
+	cin >> input;
+	for (int i = 0; i < input.size(); i++)
+	{
+		if (input[i] == 'A') 
+		{
+			temp = arr[0];
+			arr[0] = arr[1];
+			arr[1] = temp;
+		}
+		else if (input[i] == 'B')
+		{
+			temp = arr[1];
+			arr[1] = arr[2];
+			arr[2] = temp;
+		}
+		else if (input[i] == 'C')
+		{
+			temp = arr[2];
+			arr[2] = arr[0];
+			arr[0] = temp;
+		}
+	}
+	for (int i = 0; i < 3; i++)
+	{
+		if (arr[i] == 1)
+		{
+			cout << i + 1;
+		}
+	}
+}
