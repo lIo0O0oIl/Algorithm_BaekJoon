23612038fdd356a79caeebc5eec1b8cb1a36349b
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2026-01-29 08:59:04 +0900

    이중 반복문 풂

---
+++ BaekJoon/29 이중 반복문 34849.cpp
@@ -0,0 +1,12 @@
+// https://www.acmicpc.net/problem/34849
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	if (n > 10000) cout << "Time limit exceeded";
+	else cout << "Accepted";
+}
