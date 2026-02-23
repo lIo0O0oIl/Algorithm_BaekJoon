a885b4670f85935ad1e9608ee9464dc6e7a78b94
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2026-02-23 09:46:04 +0900

    end commit

---
+++ BaekJoon/end.cpp
@@ -0,0 +1,100 @@
+// 마지막 백준. 777.
+
+// 17일 브론즈5 문제 하나 풀었었음
+
+// 18일 풂
+// https://www.acmicpc.net/problem/23235
+
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//int main()
+//{
+//	int count = 1;
+//	string n;
+//	while (true)
+//	{
+//		getline(cin, n);
+//		if (n == "0") break;
+//		cout << "Case " << count << ": Sorting... done!\n";
+//		count++;
+//	}
+//}
+
+// 19일
+// https://www.acmicpc.net/problem/33179
+//
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int n, temp, count = 0;
+//	cin >> n;
+//	for (int i = 0; i < n; i++)
+//	{
+//		cin >> temp;
+//		count += temp / 2 + temp % 2;
+//	}
+//	cout << count;
+//}
+
+
+// 20일
+// ? 브론즈 5 였는디...
+
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//
+//int main()
+//{
+//	int n, d, all = 0;
+//	cin >> n >> d;
+//	vector<int> arr(n);
+//	for (int i = 0; i < n; i++)
+//	{
+//		cin >> arr[i];
+//		all += arr[i];
+//	}
+//	int price = d / all;
+//	for (int i = 0; i < n; i++)
+//	{
+//		cout << arr[i] * price << '\n';
+//	}
+//}
+
+
+// 21일
+// https://www.acmicpc.net/problem/35277
+
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int n;
+//	cin >> n;
+//	cout << n / 2000;
+//}
+
+
+// 22일
+// https://www.acmicpc.net/problem/34236
+
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//
+//int main()
+//{
+//	int n;
+//	cin >> n;
+//	vector<int> arr(n);
+//	for (int i = 0; i < n; i++)
+//	{
+//		cin >> arr[i];
+//	}
+//	cout << arr[n - 1] + (arr[1] - arr[0]);
+//}
